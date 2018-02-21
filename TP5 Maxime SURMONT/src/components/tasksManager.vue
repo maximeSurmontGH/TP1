17b0583dd6743decb5c8ae2c4094bbee1114e248
maximeSurmontBB <maxime.surmont@hei.yncrea.fr>
2018-02-20 23:13:39 -0500

    style: deleteIcon was too big

--- TP5 Maxime SURMONT/src/components/tasksManager.vue
+++ TP5 Maxime SURMONT/src/components/tasksManager.vue
@@ -103,136 +103,136 @@
 </script>
 
 <style lang='scss' scoped>
-    $document-font: 'Raleway', sans-serif;
-    $container-width: 350px;
+  $document-font: 'Raleway', sans-serif;
+  $container-width: 350px;
 
-    html, body{
-        padding: 0;
-        margin: 0;
-        font-family: $document-font;
-    }
+  html, body{
+    padding: 0;
+    margin: 0;
+    font-family: $document-font;
+  }
 
-    /* ASIDE */
-    aside{
-        height: fit-content;
+  /* ASIDE */
+  aside{
+    height: fit-content;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 180px;
+    z-index: 999;
+
+    >div{
+      margin: 10px;
+      padding: 5px;
+      font-size: 12px;
+      border: 1px solid #2e6da4;
+      border-radius: 4px;
+      position: relative;
+      color: #FFF;
+      background-color: #337ab7;
+      min-height: 30px;
+
+      >span:nth-child(3){
+        display: block;
+        word-break: break-all;
+        width: 130px;
+      }
+
+      .deleteIcon{
+        font-size: 21px;
         position: absolute;
-        top: 0;
-        right: 0;
-        width: 180px;
-        z-index: 999;
+        top: 10px;
+        right: 5px;
+        line-height: 0;
+        cursor: pointer;
+      }
 
-        >div{
-            margin: 10px;
-            padding: 5px;
-            font-size: 12px;
-            border: 1px solid #2e6da4;
-            border-radius: 4px;
-            position: relative;
-            color: #FFF;
-            background-color: #337ab7;
-            min-height: 30px;
-
-            >span:nth-child(3){
-                display: block;
-                word-break: break-all;
-                width: 130px;
-            }
-
-            .deleteIcon{
-                font-size: 30px;
-                position: absolute;
-                top: 10px;
-                right: 5px;
-                line-height: 0;
-                cursor: pointer;
-            }
-
-            .putIcon{
-                font-size: 17px;
-                position: absolute;
-                top: 30px;
-                right: 4px;
-                line-height: 0;
-                cursor: pointer;
-            }
-        }
+      .putIcon{
+        font-size: 17px;
+        position: absolute;
+        top: 30px;
+        right: 4px;
+        line-height: 0;
+        cursor: pointer;
+      }
     }
+  }
 
-    /* SECTION */
-    #container{
-        height: 100vh;
+  /* SECTION */
+  #container{
+    height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    background-image: radial-gradient(#FFFFFF 0%, #FEFEFF 85%);
+
+    >div{
+      margin-top: 30vh;
+      background: rgba(10, 10, 30, 0.15);
+      border-radius: 10px;
+      padding: 15px;
+      height: fit-content;
+
+      >p{
+        color: #FFF;
+        font-size: 30px;
+        text-align: center;
+        margin: 10px 0;
+      }
+
+      >div{
         display: flex;
         flex-wrap: wrap;
         justify-content: center;
-        background-image: radial-gradient(#FFFFFF 0%, #FEFEFF 85%);
+        width: 294px;
 
-        >div{
-            margin-top: 30vh;
-            background: rgba(10, 10, 30, 0.15);
-            border-radius: 10px;
-            padding: 15px;
-            height: fit-content;
-
-            >p{
-                color: #FFF;
-                font-size: 30px;
-                text-align: center;
-                margin: 10px 0;
-            }
-
-            >div{
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: center;
-                width: 294px;
-
-                >input{
-                    width: $container-width;
-                    height: 30px;
-                    padding: 3px 5px;
-                    border: none;
-                    font-size: 14px;
-                    margin-bottom: 15px;
-                    font-family: $document-font;
-                    border-radius: 4px;
-                }
-
-                >input:focus{
-                    outline: none;
-                }
-
-                >textarea{
-                    width: $container-width;
-                    padding: 3px 5px;
-                    border: none;
-                    font-size: 14px;
-                    resize: vertical;
-                    height: 100px;
-                    margin-bottom: 15px;
-                    font-family: $document-font;
-                    line-height: 30px;
-                    border-radius: 4px;
-                }
-
-                >button{
-                    font-size: 14px;
-                    width: 300px;
-                    padding: 5px;
-                    cursor: pointer;
-                    border: 1px solid transparent;
-                    border-radius: 4px;
-                }
-
-                >.btn-info{
-                    color: #fff;
-                    background-color: #337ab7;
-                    border-color: #2e6da4;
-                }
-
-                >.btn-info:hover{
-                    background-color: #2e6da4;
-                }
-            }
+        >input{
+          width: $container-width;
+          height: 30px;
+          padding: 3px 5px;
+          border: none;
+          font-size: 14px;
+          margin-bottom: 15px;
+          font-family: $document-font;
+          border-radius: 4px;
         }
+
+        >input:focus{
+          outline: none;
+        }
+
+        >textarea{
+          width: $container-width;
+          padding: 3px 5px;
+          border: none;
+          font-size: 14px;
+          resize: vertical;
+          height: 100px;
+          margin-bottom: 15px;
+          font-family: $document-font;
+          line-height: 30px;
+          border-radius: 4px;
+        }
+
+        >button{
+          font-size: 14px;
+          width: 300px;
+          padding: 5px;
+          cursor: pointer;
+          border: 1px solid transparent;
+          border-radius: 4px;
+        }
+
+        >.btn-info{
+          color: #fff;
+          background-color: #337ab7;
+          border-color: #2e6da4;
+        }
+
+        >.btn-info:hover{
+          background-color: #2e6da4;
+        }
+      }
     }
+  }
 </style>
