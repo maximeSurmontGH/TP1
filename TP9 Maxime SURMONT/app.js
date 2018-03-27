43eb92ffd6c0fb0b26f5c9d865c41c3b78cd6789
maximeSurmontBB <maxime.surmont@hei.yncrea.fr>
2018-03-27 16:40:54 -0400

    info: for stckovfl

--- TP9 Maxime SURMONT/app.js
+++ TP9 Maxime SURMONT/app.js
@@ -1,14 +1,6 @@
 let fs = require('fs');
 let https = require('https');
 let express = require('express');
-/*let path = require('path');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
-
-let login = require('./routes/login');
-let chat = require('./routes/chat');
-let users = require('./routes/users');*/
 
 let app = express();
 
@@ -24,36 +16,7 @@ var server = https.createServer(options, app);
 // web socket
 var io = require('socket.io')(server);
 
-/*
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 // router
-app.get('/', function(req, res) {
-	res.sendfile("./public/views/login.html");
-});
-app.get('/login', function(req, res) {
-	res.sendfile("./public/views/login.html");
-});
-app.get('/index', function(req, res) {
-	res.sendfile("./public/views/login.html");
-});
-app.get('/loginError', function(req, res) {
-	res.sendfile("./public/views/loginError.html");
-});
-app.post('/disconnect', function(req, res) {
-	res.cookie('token', '', {maxAge: 0});
-	res.send();
-});
-
-app.use('/login', login);
-app.use('/users', users);
-app.use('/chat', chat);*/
-
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
